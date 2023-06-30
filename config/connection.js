10a0f86647e1f57379699bc5a36f344cd69438e3
Criste Phillips <criste.phillips@yahoo.com>
2023-06-30 16:50:16 -0400

    added initial coding required to define sequelize parameters

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,21 @@
 // define sequelize in a manner to use jaws DB to use DB as a service as opposed to locally
 
+const Sequelize = require('sequelize');
+
+// require dotenv (zero dependency module that loads environment variables) as early as possible in application; using ES6 format
+require('doenv').config();
+
+// establish a connection to the jaws DB
+const sequelize = process.env.JAWSDB_URL    
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize (
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306,
+        }
+    );
+module.exports= sequelize;
